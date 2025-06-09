1c7ca10337b71090f64ab69564cf69be6229dbe6
serkanakin <sserkan.akin08@gmail.com>
2025-06-09 17:14:22 +0200

    fix to data path in run

--- tests/run_uq_baselines.sh
+++ tests/run_uq_baselines.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 cd tests
-
+DATA_ROOT="$HOME/projects/laplace-v2.0/data"
 ## =========================================================
 ##  R-MNIST
 ## =========================================================
 #for seed in 6 12 13 523 972394; do
 #
-#  python uq.py --data_root ~/Datasets --benchmark R-MNIST --model LeNet \
+#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST --model LeNet \
 #         --models_root models --method map --model_seed $seed
 #
-#  python uq.py --data_root ~/Datasets --benchmark R-MNIST --model LeNet \
+#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST --model LeNet \
 #         --models_root models --method ensemble --nr_components 5 --model_seed $seed
 #
-#  python uq.py --data_root ~/Datasets --benchmark R-MNIST \
+#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST \
 #         --model LeNet-BBB-flipout --models_root models/bbb/flipout \
 #         --method bbb --model_seed $seed
 #
-#  python uq.py --data_root ~/Datasets --benchmark R-MNIST \
+#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST \
 #         --model LeNet-CSGHMC --models_root models/csghmc \
 #         --method csghmc --model_seed $seed
 #
@@ -27,17 +27,17 @@ cd tests
 # =========================================================
 for seed in 6 12 13 523 972394; do
 
-  python uq.py --data_root ~/Datasets/alt --benchmark CIFAR-10-C --model WRN16-4 \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C --model WRN16-4 \
          --models_root models --method map --model_seed $seed
 
-  python uq.py --data_root ~/Datasets/alt --benchmark CIFAR-10-C --model WRN16-4 \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C --model WRN16-4 \
          --models_root models --method ensemble --nr_components 5 --model_seed $seed
 
-  python uq.py --data_root ~/Datasets/alt --benchmark CIFAR-10-C \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C \
          --model WRN16-4-BBB-flipout --models_root models/bbb/flipout \
          --method bbb --model_seed $seed
 
-  python uq.py --data_root ~/Datasets/alt --benchmark CIFAR-10-C \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C \
          --model WRN16-4-CSGHMC --models_root models/csghmc \
          --method csghmc --model_seed $seed
 
@@ -48,17 +48,17 @@ done
 # =========================================================
 for seed in 6 12 13 523 972394; do
 
-  python uq.py --data_root ~/Datasets --benchmark MNIST-OOD --model LeNet \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
          --models_root models --method map --model_seed $seed
 
-  python uq.py --data_root ~/Datasets --benchmark MNIST-OOD --model LeNet \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
          --models_root models --method ensemble --nr_components 5 --model_seed $seed
 
-  python uq.py --data_root ~/Datasets --benchmark MNIST-OOD \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD \
          --model LeNet-BBB-flipout --models_root models/bbb/flipout \
          --method bbb --model_seed $seed
 
-  python uq.py --data_root ~/Datasets --benchmark MNIST-OOD \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD \
          --model LeNet-CSGHMC --models_root models/csghmc \
          --method csghmc --model_seed $seed
 
@@ -69,17 +69,17 @@ done
 # =========================================================
 for seed in 6 12 13 523 972394; do
 
-  python uq.py --data_root ~/Datasets --benchmark CIFAR-10-OOD --model WRN16-4 \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-OOD --model WRN16-4 \
          --models_root models --method map --model_seed $seed
 
-  python uq.py --data_root ~/Datasets --benchmark CIFAR-10-OOD --model WRN16-4 \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-OOD --model WRN16-4 \
          --models_root models --method ensemble --nr_components 5 --model_seed $seed
 
-  python uq.py --data_root ~/Datasets --benchmark CIFAR-10-OOD \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-OOD \
          --model WRN16-4-BBB-flipout --models_root models/bbb/flipout \
          --method bbb --model_seed $seed
 
-  python uq.py --data_root ~/Datasets --benchmark CIFAR-10-OOD \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-OOD \
          --model WRN16-4-CSGHMC --models_root models/csghmc \
          --method csghmc --model_seed $seed
 
@@ -88,8 +88,8 @@ done
 # =========================================================
 #  SWAG
 # =========================================================
-python uq.py --data_root ~/Datasets --benchmark MNIST-OOD --model LeNet \
+python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
        --models_root models --method swag --n_samples 30 --seed 711 --model_seed 6
 
-python uq.py --data_root ~/Datasets --benchmark CIFAR-10-OOD --model WRN16-4 \
+python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-OOD --model WRN16-4 \
        --models_root models --method swag --n_samples 30 --seed 711 --model_seed 6
