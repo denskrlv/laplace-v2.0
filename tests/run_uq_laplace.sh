7240be703703b95f25e91f24507ce83f22ecf3d1
serkanakin <sserkan.akin08@gmail.com>
2025-06-10 13:17:00 +0200

    laplace script, fixed data root path

--- tests/run_uq_laplace.sh
+++ tests/run_uq_laplace.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 cd tests
 
+DATA_ROOT="$HOME/projects/laplace-v2.0/data"
+#DATA_ROOT="data"
+
 # =========================================================
 #  R-MNIST
 # =========================================================
-for seed in 6 12 13 523 972394; do
-#  python uq.py --data_root ~/Datasets --benchmark R-MNIST --model LeNet \
-#         --models_root models --method laplace --model_seed $seed --subset_of_weights all
-
-  python uq.py --data_root ~/Datasets --benchmark R-MNIST --model LeNet \
-         --models_root models --method laplace --model_seed $seed --subset_of_weights last_layer
-done
+#for seed in 6 12 13 523 972394; do
+##  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST --model LeNet \
+##         --models_root models --method laplace --model_seed $seed --subset_of_weights all
+#
+#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST --model LeNet \
+#         --models_root models --method laplace --model_seed $seed --subset_of_weights last_layer
+#done
 
 # =========================================================
 #  CIFAR-10-C
 # =========================================================
 for seed in 6 12 13 523 972394; do
-#  python uq.py --data_root ~/Datasets/alt --benchmark CIFAR-10-C --model WRN16-4 \
+#  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C --model WRN16-4 \
 #         --models_root models --method laplace --model_seed $seed --subset_of_weights all
-  python uq.py --data_root ~/Datasets/alt --benchmark CIFAR-10-C --model WRN16-4 \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C --model WRN16-4 \
          --models_root models --method laplace --model_seed $seed --subset_of_weights last_layer
 done
 
@@ -26,9 +29,9 @@ done
 #  MNIST OOD DETECTION
 # =========================================================
 for seed in 6 12 13 523 972394; do
-#  python uq.py --data_root ~/Datasets --benchmark MNIST-OOD --model LeNet \
+#  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
 #         --models_root models --method laplace --model_seed $seed --subset_of_weights all
-  python uq.py --data_root ~/Datasets --benchmark MNIST-OOD --model LeNet \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
          --models_root models --method laplace --model_seed $seed  --subset_of_weights last_layer
 done
 
@@ -36,8 +39,8 @@ done
 #  CIFAR-10 OOD DETECTION
 # =========================================================
 for seed in 6 12 13 523 972394; do
-#  python uq.py --data_root ~/Datasets --benchmark CIFAR-10-OOD --model WRN16-4 \
+#  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-OOD --model WRN16-4 \
 #         --models_root models --method laplace --model_seed $seed --subset_of_weights all
-  python uq.py --data_root ~/Datasets --benchmark CIFAR-10-OOD --model WRN16-4 \
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-OOD --model WRN16-4 \
          --models_root models --method laplace --model_seed $seed --subset_of_weights last_layer
 done
