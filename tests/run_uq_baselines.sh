4f2576fa5982a6a83b6f50005627023506e02914
serkanakin <sserkan.akin08@gmail.com>
2025-06-10 12:53:36 +0200

    baselines script updated (complete)

--- tests/run_uq_baselines.sh
+++ tests/run_uq_baselines.sh
@@ -2,68 +2,68 @@
 cd tests
 DATA_ROOT="$HOME/projects/laplace-v2.0/data"
 #DATA_ROOT="data"
-## =========================================================
-##  R-MNIST
-## =========================================================
-#for seed in 6 12 13 523 972394; do
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST --model LeNet \
-#         --models_root models --method map --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST --model LeNet \
-#         --models_root models --method ensemble --nr_components 5 --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST \
-#         --model LeNet-BBB-flipout --models_root models/bbb/flipout \
-#         --method bbb --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST \
-#         --model LeNet-CSGHMC --models_root models/csghmc \
-#         --method csghmc --model_seed $seed
-#
-#done
+# =========================================================
+#  R-MNIST
+# =========================================================
+for seed in 6 12 13 523 972394; do
 
+  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST --model LeNet \
+         --models_root models --method map --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST --model LeNet \
+         --models_root models --method ensemble --nr_components 5 --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST \
+         --model LeNet-BBB-flipout --models_root models/bbb/flipout \
+         --method bbb --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark R-MNIST \
+         --model LeNet-CSGHMC --models_root models/csghmc \
+         --method csghmc --model_seed $seed
+
+done
+#
 # =========================================================
 #  CIFAR-10-C
 # =========================================================
-#for seed in 6 12 13 523 972394; do
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C --model WRN16-4 \
-#         --models_root models --method map --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C --model WRN16-4 \
-#         --models_root models --method ensemble --nr_components 5 --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C \
-#         --model WRN16-4-BBB-flipout --models_root models/bbb/flipout \
-#         --method bbb --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C \
-#         --model WRN16-4-CSGHMC --models_root models/csghmc \
-#         --method csghmc --model_seed $seed
-#
-#done
-#
-## =========================================================
-##  MNIST OOD DETECTION
-## =========================================================
-#for seed in 6 12 13 523 972394; do
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
-#         --models_root models --method map --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
-#         --models_root models --method ensemble --nr_components 5 --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD \
-#         --model LeNet-BBB-flipout --models_root models/bbb/flipout \
-#         --method bbb --model_seed $seed
-#
-#  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD \
-#         --model LeNet-CSGHMC --models_root models/csghmc \
-#         --method csghmc --model_seed $seed
-#
-#done
+for seed in 6 12 13 523 972394; do
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C --model WRN16-4 \
+         --models_root models --method map --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C --model WRN16-4 \
+         --models_root models --method ensemble --nr_components 5 --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C \
+         --model WRN16-4-BBB-flipout --models_root models/bbb/flipout \
+         --method bbb --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark CIFAR-10-C \
+         --model WRN16-4-CSGHMC --models_root models/csghmc \
+         --method csghmc --model_seed $seed
+
+done
+
+# =========================================================
+#  MNIST OOD DETECTION
+# =========================================================
+for seed in 6 12 13 523 972394; do
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
+         --models_root models --method map --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD --model LeNet \
+         --models_root models --method ensemble --nr_components 5 --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD \
+         --model LeNet-BBB-flipout --models_root models/bbb/flipout \
+         --method bbb --model_seed $seed
+
+  python uq.py --data_root "${DATA_ROOT}" --benchmark MNIST-OOD \
+         --model LeNet-CSGHMC --models_root models/csghmc \
+         --method csghmc --model_seed $seed
+
+done
 
 # =========================================================
 #  CIFAR-10 OOD DETECTION
