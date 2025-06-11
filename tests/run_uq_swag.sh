8519a5e3730b3874fac7e6f8103280556199cc61
serkanakin <sserkan.akin08@gmail.com>
2025-06-11 14:55:42 +0200

    batch size reduced

--- tests/run_uq_swag.sh
+++ tests/run_uq_swag.sh
@@ -7,16 +7,16 @@ SUBSPACE_ARGS="--method swag_laplace \
                --subspace_dim ${SUBSPACE_DIM} \
                --subspace_method ${SUBSPACE_METHOD} \
                --eig_steps ${EIG_STEPS}
-               --batch_size 128"
+               --batch_size 64"
 
 DATA_ROOT="$HOME/projects/laplace-v2.0/data" #vm data
 #DATA_ROOT="data"  #local data
 
-for seed in 6 12 13 523 972394; do
-  python uq.py --data_root "$DATA_ROOT" \
-          --benchmark MNIST-OOD --model LeNet \
-          --models_root models ${SUBSPACE_ARGS} --model_seed "$seed"
-done
+#for seed in 6 12 13 523 972394; do
+#  python uq.py --data_root "$DATA_ROOT" \
+#          --benchmark MNIST-OOD --model LeNet \
+#          --models_root models ${SUBSPACE_ARGS} --model_seed "$seed"
+#done
 
 # # =========================================================
 for seed in 6 12 13 523 972394; do
