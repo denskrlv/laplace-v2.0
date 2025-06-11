cb1a914338b3c364cd8522dc25c1475fb5eb6747
denskrlv <d.krylov@student.utwente.nl>
2025-06-11 12:57:09 +0200

    Changed the code for SWAG Laplace.

--- tests/baselines/vanilla/models/wrn_fixup.py
+++ tests/baselines/vanilla/models/wrn_fixup.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from asdfghjkl.operations import Bias, Scale
+from asdl.operations import Bias, Scale
 
 
 ##########################################################################################
