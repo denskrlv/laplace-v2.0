ea3c89c50c31c0aab1babf9ca46a0f6f3ce798f9
serkanakin <sserkan.akin08@gmail.com>
2025-06-13 23:41:17 +0200

    Update run_uq_adult.sh to use python3 for executing scripts

--- tests/run_uq_adult.sh
+++ tests/run_uq_adult.sh
@@ -23,23 +23,23 @@ COMMON_ARGS="--data_root ${DATA_ROOT} \
 # === 1. BASELINE EXPERIMENTS ===
 echo ""
 echo "--> RUNNING BASELINE MAP EXPERIMENT"
-python tests/uq.py ${COMMON_ARGS} --method map --run_name "${BENCHMARK}/map_baseline"
+python3 tests/uq.py ${COMMON_ARGS} --method map --run_name "${BENCHMARK}/map_baseline"
 
 echo ""
 echo "--> RUNNING BASELINE LAPLACE (LAST-LAYER) EXPERIMENT"
 # This corresponds to the default "LA" in the paper
-python tests/uq.py ${COMMON_ARGS} --method laplace --subset_of_weights last_layer --run_name "${BENCHMARK}/laplace_ll_baseline"
+python3 tests/uq.py ${COMMON_ARGS} --method laplace --subset_of_weights last_layer --run_name "${BENCHMARK}/laplace_ll_baseline"
 
 
 # === 2. DOMAIN SHIFT EXPERIMENTS ===
 echo ""
 echo "------------------------------------------------"
 echo "--> RUNNING DOMAIN SHIFT: MALE-TO-FEMALE"
-python tests/uq.py ${COMMON_ARGS} --method laplace --subset_of_weights last_layer --domain_shift_gender male_to_female --run_name "${BENCHMARK}/shift_male_to_female"
+python3 tests/uq.py ${COMMON_ARGS} --method laplace --subset_of_weights last_layer --domain_shift_gender male_to_female --run_name "${BENCHMARK}/shift_male_to_female"
 
 echo ""
 echo "--> RUNNING DOMAIN SHIFT: FEMALE-TO-MALE"
-python tests/uq.py ${COMMON_ARGS} --method laplace --subset_of_weights last_layer --domain_shift_gender female_to_male --run_name "${BENCHMARK}/shift_female_to_male"
+python3 tests/uq.py ${COMMON_ARGS} --method laplace --subset_of_weights last_layer --domain_shift_gender female_to_male --run_name "${BENCHMARK}/shift_female_to_male"
 
 
 # === 3. NOISE INTENSITY EXPERIMENTS ===
@@ -49,7 +49,7 @@ echo "--> RUNNING NOISE INTENSITY EXPERIMENTS"
 for intensity in 0.1 0.25 0.5 0.75 1.0; do
     echo ""
     echo "    -> Noise Intensity: ${intensity}"
-    python tests/uq.py ${COMMON_ARGS} --method laplace --subset_of_weights last_layer --noise_intensity ${intensity} --run_name "${BENCHMARK}/noise_${intensity}"
+    python3 tests/uq.py ${COMMON_ARGS} --method laplace --subset_of_weights last_layer --noise_intensity ${intensity} --run_name "${BENCHMARK}/noise_${intensity}"
 done
 
 echo ""
